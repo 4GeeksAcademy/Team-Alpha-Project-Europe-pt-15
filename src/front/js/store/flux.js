dea87b822df55b9f4297097f53dffc9d20c01f60
ElioAloise <155248934+ElioAloise@users.noreply.github.com>
2024-05-18 17:11:56 +0000

    encounter logic done

--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -1,3 +1,5 @@
+import React, { useState, useEffect } from "react";
+
 const getState = ({ getStore, getActions, setStore }) => {
 	return {
 		store: {
@@ -51,7 +53,7 @@ const getState = ({ getStore, getActions, setStore }) => {
   				.catch((error) => console.error(error));
 			},
 			getMonsterByCr: (challengeRating1,challengeRating2,challengeRating3,challengeRating4,challengeRating5,challengeRating6,challengeRating7,challengeRating8,challengeRating9,challengeRating10,challengeRating11,challengeRating12,challengeRating13,challengeRating14,challengeRating15,challengeRating16,challengeRating17,challengeRating18,challengeRating19,challengeRating20,challengeRating21,challengeRating22,challengeRating23,challengeRating24,challengeRating25,challengeRating26)=>{
-				//monster challenge ranting goes like this 0.125, 0.250 , 0.500 and the form 1 to 24
+				//monster challenge ranting goes like this 0.125, 0.250 , 0.500 and then form 1 to 24
 				const store=getStore()
 				const myHeaders = new Headers();
 				myHeaders.append("Accept", "application/json");
@@ -105,6 +107,10 @@ const getState = ({ getStore, getActions, setStore }) => {
 				if(userLevel <= 140) {return action.getMonsterByCr(0.125,0.250,0.500,1,2,3,4,5,6,7,8,9,10,11)}
 				if(userLevel <= 150) {return action.getMonsterByCr(0.125,0.250,0.500,1,2,3,4,5,6,7,8,9,10,11,12)}
 				if(userLevel <= 160) {return action.getMonsterByCr(0.125,0.250,0.500,1,2,3,4,5,6,7,8,9,10,11,12,13)}
+				if(userLevel <= 170) {return action.getMonsterByCr(0.125,0.250,0.500,1,2,3,4,5,6,7,8,9,10,11,12,13,14)}
+				if(userLevel <= 180) {return action.getMonsterByCr(0.125,0.250,0.500,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15)}
+				if(userLevel <= 190) {return action.getMonsterByCr(0.125,0.250,0.500,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16)}
+				if(userLevel <= 200) {return action.getMonsterByCr(0.125,0.250,0.500,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24)}
 			
 			},
 			getBestiary: async (userId)=>{
@@ -122,22 +128,22 @@ const getState = ({ getStore, getActions, setStore }) => {
 					console.log("Error loading message from backend", error)
 				}	
 			},
-			decideEncounter: (userlvl,userid)=>{
+			decideEncounter: (userlvl,userId)=>{
 				const store=getStore()
 				const action=getActions()
 				action.getEncounter(userlvl)
-				action.getBestiary(userid)
-
+				action.getBestiary(userId)
+				
 				setTimeout(() => {
-					const encounterPool = store.encounterPool?.map((item)=>{return item.index});
-					const bestiary = store.bestiary?.map((item)=>{return item.monster_name});
-					const monsterpool = encounterPool.filter(val => !bestiary.includes(val));
-					const randomMonster = monsterpool[Math.floor(Math.random() * monsterpool.length)]
-					setStore({randomMonster: randomMonster})
-				  }, "200");
+				const encounterPool = store.encounterPool?.map((item)=>{return item.index});
+				const bestiary = store.bestiary?.map((item)=>{return item.monster_name});
+				const monsterpool = encounterPool.filter(val => !bestiary.includes(val));
+				const randomMonster = monsterpool[Math.floor(Math.random() * monsterpool.length)]
+				setStore({randomMonster: randomMonster})
+				  }, "500");
 				
 			},
-			addMosnter:async (userId)=>{
+			addMosnterOnBestiary:async (userId)=>{
 				const store=getStore()
 				const action=getActions()
 				
@@ -162,12 +168,18 @@ const getState = ({ getStore, getActions, setStore }) => {
 					   console.log(error);
 				   });
 			},
-			decideVictory: ()=>{
-				const playerDice = Math.floor(Math.random() * 6) + 1;
-				const monsterDice =Math.floor(Math.random() * 6) + 1;
-
-				if(playerDice > monsterDice){return console.log(true)}
-				else{return console.log(false)}
+			decideVictory: async (userLevel,userId)=>{
+				const store=getStore()
+				const action=getActions()
+				action.decideEncounter(userLevel,userId)
+				
+				setTimeout(() => {
+					console.log(store.randomMonster)
+					const playerDice = Math.floor(Math.random() * 6) + 1
+					const monsterDice =Math.floor(Math.random() * 6) + 1;
+					if(playerDice > monsterDice){return action.addMosnterOnBestiary(userId) }
+					else{return console.log(false)}
+				}, "500");
 			}
 
 		}
