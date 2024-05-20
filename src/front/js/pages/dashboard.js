e56b750a8ff5468ed033984a13b97bc805c4433f
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-05-20 23:30:48 +0000

    added css file for dashboard

--- src/front/js/pages/dashboard.js
+++ src/front/js/pages/dashboard.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/index.css";
+import "../../styles/dashboard.css";
 import { Link } from "react-router-dom";
 
 
@@ -9,8 +10,7 @@ export const Dash = () => {
   
   const {tasks} = store;
   const {addTasks, getTask, updateTask, getUsers, getLevel, handleChange, label } = actions;
-  //user for test
-  const user = 1
+ 
 
 
   return (
@@ -19,22 +19,24 @@ export const Dash = () => {
         <div className="d-flex ">
         
         <div className="card bg-red">
-        <div className="m-2">{store.user[id]}</div>
-        <div className="m-2">{store.user.Level}</div>
-        <div className="mt-2">{store.user.roles}</div>
-        <div className="mt-2">{store.user.energy}</div>
-        <div className="mt-2">{store.user.experience}</div>
+        <div className="m-2">username{store.user}</div>
+        <div className="m-2">Lvl{store.user.Level}</div>
+        <div className="mt-2">Role{store.user.roles}</div>
+        <div className="mt-2">energy{store.user.energy}</div>
+        <div className="mt-2">xp{store.user.experience}</div>
         
-        <div className=""><div className="carangueijo"></div><div className="something else"></div></div>
+        <div className="">
+          <div className="carangueijo"></div>
+        <div className="something else"></div></div>
         <div className="">Edit Profile</div>
-      </div>
-      <div className="">
+        </div>
+        <div className="">
         <div className=""><div className="">quests</div><div className="">rewards</div></div>
 
         <div className="">actions.getTask()</div>
-      </div>
+        </div>
       
-      </div>
+        </div>
 
       
       </div>
--- src/front/js/pages/insertask.js
+++ src/front/js/pages/insertask.js
@@ -10,47 +10,47 @@ export const Tasker = () => {
     const {tasks} = store;
 
         return <div className="container-fluid text-center"> 
-        <div className="row bg-custom-purple">
-        <div className="col h-100 align-self-center">
+            <div className="row bg-custom-purple">
+             <div className="col h-100 align-self-center">
 
-        <div className="card col-lg-3 d-lg-inline-flex m-3 gap-3 card">
+            <div className="card col-lg-3 d-lg-inline-flex m-3 gap-3 card">
 
-        <div className="title"><h1 className="quest text-center font-weight-bold"> New Quest</h1></div>
+            <div className="title"><h1 className="quest text-center font-weight-bold"> New Quest</h1></div>
 
-        <div className="card">Name your quest over here
-        <input className="inpuTasker form-control border-0 shadow-none"
-        type="text"
-        //value="label"
-        //onChange={}
+            <div className="card">Name your quest over here!
+            <textarea className="inpuTasker form-control border-0 shadow-none"
+            type="text"
+            required
+            //maxLength={}
+            //value={label}
+            //onChange={}
 
-        ></input>
-        </div>
+            ></textarea>
+            </div>
         
-        <div className="dif-title"><h4 className="quest align-items-center mb-0 mt-3"> Difficulty</h4></div>
+            <div className="dif-title"><h4 className="quest align-items-center mb-0 mt-3"> Difficulty</h4></div>
 
-        <div className="d-flex justify-content-evenly"> 
+            <div className="d-flex justify-content-evenly"> 
         
-        <div className="btn bttn"
-        //onclick={}
-        >Easy</div> 
-        <div className="btn bttn"
-        //onclick={}
-        >Medium</div> 
-        <div className="btn bttn"
-        //onclick={}
-        >Hard</div>
-        </div>
+            <div className="btn bttn"
+            //onclick={}
+            >Easy</div> 
+            <div className="btn bttn"
+            //onclick={}
+            >Medium</div> 
+            <div className="btn bttn"
+            //onclick={}
+            >Hard</div>
+            </div>
 
-        <div className="btn bttn text-center bg-custom-yellow m-2 mt-3"
-        //onclick={store.actions.addtask}
-        ><h3>Add it!</h3> </div>
+            <div className="btn bttn text-center bg-custom-yellow m-2 mt-3"
+            //onclick={store.actions.addtask}
+            ><h3 className="addTaskBtn">Add it!</h3> </div>
 
 
         
+            </div>
         </div>
-        </div>
-        </div>
-
-        </div>
-    
+    </div>
+</div>
 }
