327a4e75dc2a11a87f852b21ed4eab8cfc94f39c
joaoaugusto-jesus <147534481+joaoaugusto-jesus@users.noreply.github.com>
2024-05-30 14:47:14 +0000

    tasker fully working with styles and queries

--- src/front/js/component/CreateTaskCard/DifficultyButtons.js
+++ src/front/js/component/CreateTaskCard/DifficultyButtons.js
@@ -2,16 +2,17 @@ import React, { useState, useEffect, useContext } from "react";
 import { Context } from "../../store/appContext";
 import "./styles.css";
 
-const DifficultyButton = ({ taskDifficulty, setTaskDifficulty, number, text }) => {
+const DifficultyButton = ({ taskDifficulty, setTaskDifficulty, number, text, icone }) => {
     const isThisSelected = number == taskDifficulty
     const backgroundColor = isThisSelected ? "black" : "white"
     const textColor = isThisSelected ? "white" : "black"
+
 
     return <button className={"btnDif card btn "} style={{ backgroundColor }}
         onClick={() => setTaskDifficulty(number)}
     >
         <div className=" btn-content">
-            <i className="fa-regular fa-star" style={{ color: textColor }}></i>
+            <i className={icone} style={{ color: textColor }}></i>
             <div className={`label `} style={{ color: textColor }}>{text}</div>
         </div>
 
@@ -29,9 +30,9 @@ const DifficultyButtons = ({ taskDifficulty, setTaskDifficulty }) => {
 
         <div className="d-flex justify-content-evenly difSelector">
 
-            <DifficultyButton taskDifficulty={taskDifficulty} setTaskDifficulty={setTaskDifficulty} number={1} text="Easy" />
-            <DifficultyButton taskDifficulty={taskDifficulty} setTaskDifficulty={setTaskDifficulty} number={2} text="Medium" />
-            <DifficultyButton taskDifficulty={taskDifficulty} setTaskDifficulty={setTaskDifficulty} number={3} text="Hard" />
+            <DifficultyButton taskDifficulty={taskDifficulty} setTaskDifficulty={setTaskDifficulty} number={1} text="Easy" icone={"fa-regular fa-star"} />
+            <DifficultyButton taskDifficulty={taskDifficulty} setTaskDifficulty={setTaskDifficulty} number={2} text="Medium" icone={"fa-solid fa-star-half-stroke"} />
+            <DifficultyButton taskDifficulty={taskDifficulty} setTaskDifficulty={setTaskDifficulty} number={3} text="Hard" icone={"fa-solid fa-star"} />
 
         </div>
     </>
